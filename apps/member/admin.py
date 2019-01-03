e19f0b7e9824c22fd8973d1113df97922976707e
lbssone <loveseo1124@gmail.com>
2019-01-03 23:29:29 +0800

    no transaction

--- apps/member/admin.py
+++ apps/member/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
-from .models import Member, Transaction, Transaction_product
+from .models import Member
 
 # Register your models here.
 admin.site.register(Member)
-admin.site.register(Transaction)
-admin.site.register(Transaction_product)
+
